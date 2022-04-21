6e9846d469ea7d9592fd82a836a768cad2ae07ec
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-04-20 21:27:22 -0700

    Write SELECT statements to retrieve certain info

---
+++ 1_queries/01_user_login.sql
@@ -0,0 +1,7 @@
+/*
+Get details about a single user
+*/
+
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
---
+++ 1_queries/02_avg_reso_length.sql
@@ -0,0 +1,6 @@
+/*
+Get the average duration of all reservations
+*/
+
+SELECT avg(end_date - start_date) as average_duration
+FROM reservations;
